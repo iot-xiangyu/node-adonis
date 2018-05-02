b9c7c91ccb14872eac966820e6eafd9f87cf8d52
iot-xiangyu <iot_xiexiangyu@163.com>
2018-05-02 06:34:25 +0000

    basic

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Database = use('Database')
+
+class PostController {
+  async index () {
+  }
+
+  async create ({ view }) {
+    return view.render('post.create')
+  }
+
+  async store ({ request, response }) {
+    const newPost = request.only(['title', 'content'])
+    const postID = await Database.insert(newPost).into('posts')
+    console.log('postID:', postID)
+    return response.redirect(`/posts/${ postID[0] }`)
+  }
+
+  async show ({ view, params }) {
+    const post = await Database.from('posts').where('id', params.id).first()
+    return view.render('post.show', { post })
+  }
+
+  async edit ({ view, params }) {
+    const post = await Database.from('posts').where('id', params.id).first()
+    return view.render('post.edit', { post })
+  }
+
+  async update ({ request, params }) {
+    const updatedPost = request.only(['title', 'content'])
+    await Database.table('posts').where('id',params.id).update(updatedPost)
+  }
+
+  async destroy ({ request, params }) {
+    await Database.table('posts').where('id', params.id).delete()
+    return 'success'
+  }
+}
+
+module.exports = PostController
